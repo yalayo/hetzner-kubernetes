9ff7316ca869bc5d165801ac17f4655fa1c11d7a
Yunior <yunior.alayo@gmail.com>
2025-07-25 18:50:06 +0200

    Updated the user_data to only install nix.

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,14 +92,7 @@ resource "hcloud_server" "master" {
     #!/bin/bash
     sudo apt-get update
     sudo apt-get -y install ca-certificates curl
-    sudo install -m 0755 -d /etc/apt/keyrings
-    sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-    sudo chmod a+r /etc/apt/keyrings/docker.asc
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update
-    sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo sed -i 's/^#*\s*PermitRootLogin\s.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
-    sudo systemctl restart ssh
+    sudo sh <(curl --proto '=https' --tlsv1.2 -L https://nixos.org/nix/install) --daemon
   EOF
 
   public_net {
