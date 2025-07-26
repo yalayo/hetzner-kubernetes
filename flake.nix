08a1b6ae7db9ea01cc8c6e207d35f300f4c7ae11
Yunior <yunior.alayo@gmail.com>
2025-07-26 11:07:01 +0200

    Added flake to work with nix on the workflow.

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Terraform + nixos-anywhere";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.terraform
+            pkgs.nix
+            pkgs.git
+            pkgs.curl
+          ];
+        };
+      });
+}
