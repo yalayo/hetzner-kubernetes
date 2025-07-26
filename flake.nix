dc4b40877153e230e5d3a7a60b84f19abcc775fd
Yunior <yunior.alayo@gmail.com>
2025-07-26 11:15:58 +0200

    Change to allow packages with unfree licenses.

--- flake.nix
+++ flake.nix
@@ -9,7 +9,10 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = [
