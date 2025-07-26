098d3b830b1210f63868ab4d7b923c86fc0dc611
Yunior <yunior.alayo@gmail.com>
2025-07-26 12:14:59 +0200

    Updated the path to allow access from the terraform module.

--- terraform/run-terraform.sh
+++ terraform/run-terraform.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Make sure nix is explicitly in PATH
+export PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:/usr/local/bin:$PATH"
+
 terraform init
 terraform apply -auto-approve
