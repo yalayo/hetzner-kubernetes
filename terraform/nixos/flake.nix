4c1710136d3e6a969d1cb8bac964b645e18bd0a9
Yunior <yunior.alayo@gmail.com>
2025-07-28 20:10:45 +0200

    Added nixos config in a folder to fix config issue.

---
+++ terraform/nixos/flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Terraform + nixos-anywhere";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    disko.url = "github:nix-community/disko";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, disko, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.mymachine = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          ./disko.nix
+          disko.nixosModules.disko
+        ];
+      };
+
+      devShells = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.terraform
+              pkgs.nix
+              pkgs.git
+              pkgs.curl
+            ];
+          };
+        });
+    };
+}
