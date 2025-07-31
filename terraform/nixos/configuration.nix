001f1baa6a8ff1166aa8d5a58324beb63f5d1c88
Yunior <yunior.alayo@gmail.com>
2025-07-31 12:40:18 +0200

    Updated the config to install k3s and configure the first node.

--- terraform/nixos/configuration.nix
+++ terraform/nixos/configuration.nix
@@ -4,16 +4,30 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "mymachine";
+  networking.hostName = "main";
+
+  networking.firewall.allowedTCPPorts = [
+    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+    2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  ];
+  networking.firewall.allowedUDPPorts = [
+    8472 # k3s, flannel: required if using multi-node for inter-node networking
+  ];
 
   time.timeZone = "UTC";
 
-  users.users.root = {
-    initialPassword = "changeme";
-  };
-
   services.openssh.enable = true;
-  services.openssh.passwordAuthentication = true;
+
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      # "--debug" # Optionally add additional args to k3s
+    ];
+    token = "testPassword";
+    clusterInit = true;
+  };
 
   environment.systemPackages = with pkgs; [
     git
