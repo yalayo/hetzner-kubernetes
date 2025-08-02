00f63a7c7e51cb96da8e2da55007284585c473e7
Yunior <yunior.alayo@gmail.com>
2025-08-02 11:15:01 +0200

    Trying to fix current error.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -9,12 +9,13 @@
   outputs = { self, nixpkgs, flake-utils, ... }:
     let
       systems = [ "aarch64-linux" "x86_64-linux" ];
+
       perSystem = flake-utils.lib.eachSystem systems (system:
         let
           pkgs = import nixpkgs { inherit system; };
         in {
           nixosConfigurations = {
-            "prod-master" = pkgs.lib.nixosSystem {
+            prod-master = pkgs.lib.nixosSystem {
               inherit system;
               modules = [ ./k3s-node.nix ];
             };
@@ -23,7 +24,26 @@
     in {
       inherit perSystem;
 
-      # Flattened convenience alias: prod-master refers to the aarch64 one.
-      prod-master = perSystem."aarch64-linux".nixosConfigurations."prod-master";
+      # expose the expected shape so nixos-anywhere can introspect on x86_64-linux too
+      packages = {
+        x86_64-linux = {
+          nixosConfigurations = {
+            prod-master = perSystem."x86_64-linux".nixosConfigurations.prod-master;
+          };
+        };
+        aarch64-linux = {
+          nixosConfigurations = {
+            prod-master = perSystem."aarch64-linux".nixosConfigurations.prod-master;
+          };
+        };
+      };
+
+      legacyPackages = packages;
+
+      # top-level convenience alias: prod-master means the aarch64 target you intend to install
+      nixosConfigurations = {
+        prod-master = perSystem."aarch64-linux".nixosConfigurations.prod-master;
+      };
+      prod-master = perSystem."aarch64-linux".nixosConfigurations.prod-master;
     };
 }
