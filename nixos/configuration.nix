50be1eb888222f6242a098769f710aca72eb6a9c
Yunior <yunior.alayo@gmail.com>
2025-08-02 21:19:39 +0200

    Fixing small syntax error.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -20,7 +20,7 @@
   users.users.root.openssh.authorizedKeys.keys =
   [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSSk8xXcuwN2Ws7tw/6avEYZbi4CUPQfEFb3EO3ea2hhHv/fccs5GzwGK1gAgVpujTUUwqceeEm9WlpEe7x7aksmn+ghBPKpgACT4VFUh/6IjySVi6PKhxhUtD4SyXEobwkKQxgKCjKv2OUD/Eu2QUK6xZwwrobDlwHv8WZr74mKs/+nugPQBVQhDmmU3aCol/emrTpCeYrQO4sy2/4LkK857zSQH777SqXgOT+8iRrkQe7NeDR/FSt/omn65LvBNcbFnh14/x/EC+UP6gKpvEapUmiJ6QXZ3gN0dmUFv3yECKrW5FPVuABjaQNtIBkJZ9bsJgna7iXKgqQMRePLxh ssh-key-2023-04-27"
-  ]
+  ];
 
   environment.systemPackages = with pkgs; [
     curl
