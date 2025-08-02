bf27f7c0f6a955d150817110be7e5a641fe268b9
Yunior <yunior.alayo@gmail.com>
2025-08-02 20:53:29 +0200

    Trying again to add the public ssh key.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -16,14 +16,18 @@
         nixos-facter-modules,
         ...
     }:
-    {
+    let
+      lib = nixpkgs.lib;
+      # Conditionally include the extra SSH key module if it was created by your workflow
+      extraKeyModule = lib.optional (builtins.pathExists ./extra-keys.nix) ./extra-keys.nix;
+    in {
         nixosConfigurations.prod-master = nixpkgs.lib.nixosSystem {
             system = "aarch64-linux";
             modules = [
                 disko.nixosModules.disko
                 ./configuration.nix
                 ./k3s-node.nix
-            ];
+            ] ++ extraKeyModule;
         };
     };
 }
