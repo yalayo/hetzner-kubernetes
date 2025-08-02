019edfd9cd58db406621bb914325064f87fb3ce5
Yunior <yunior.alayo@gmail.com>
2025-08-02 11:50:13 +0200

    Another try.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -6,21 +6,12 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    let
-      systems = [ "aarch64-linux" "x86_64-linux" ];
-      perSystem = flake-utils.lib.eachSystem systems (system:
-        let pkgs = import nixpkgs { inherit system; };
-        in {
-          nixosConfigurations = {
-            prod-master = pkgs.lib.nixosSystem {
-              inherit system;
-              modules = [ ./k3s-node.nix ];
-            };
-          };
-        });
-    in {
-      inherit perSystem;
-      nixosConfigurations = perSystem."aarch64-linux".nixosConfigurations;
+  outputs = { self, nixpkgs, ... }: {
+    nixosConfigurations = {
+      prod-master = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [ ./k3s-node.nix ];
+      };
     };
+  };
 }
