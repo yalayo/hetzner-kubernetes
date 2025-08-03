085aec72ea49a34708e0476446ae49dab84e49a9
Yunior <yunior.alayo@gmail.com>
2025-08-03 13:05:57 +0200

    File name changed and nodes creation.

--- terraform/cluster.tf
+++ terraform/cluster.tf
@@ -9,26 +9,22 @@ variable "k3s_token" {
 }
 
 ## VM
-resource "hcloud_server" "master" { 
-  name        = "prod-master"
+resource "hcloud_server" "node" { 
+  count       = 3
+  name        = "prod-node-${count.index}"
   image       = "ubuntu-24.04"
   location    = "nbg1"
-  server_type = "cax21" 
+  server_type = "cax11" 
   keep_disk   = true
   ssh_keys    = [data.hcloud_ssh_key.ssh_key.id] 
   firewall_ids = [hcloud_firewall.cluster.id]
 
   public_net {
-    ipv6_enabled = true
     ipv4_enabled = true
   }
 
   network {
     network_id = hcloud_network.network.id
-    ip         = "10.1.1.1"
-    alias_ips  = [
-      "10.1.1.2"
-    ]
   }
 
   depends_on = [
@@ -36,6 +32,12 @@ resource "hcloud_server" "master" {
   ]
 }
 
-output "master_ip" {
-  value = hcloud_server.master.ipv4_address
+output "nodes_ips" {
+  value = [for s in hcloud_server.node : s.ipv4_address]
+  description = "List of public IPv4 addresses of the three nodes"
+}
+
+output "first_node_ip" {
+  value       = hcloud_server.node[0].ipv4_address
+  description = "Bootstrap (first) node, used for --cluster-init"
 }
