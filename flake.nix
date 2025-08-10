79452ecd01c3644f3ee180bec9dadf6ac19799a4
Yunior <yunior.alayo@gmail.com>
2025-08-10 11:38:27 +0200

    Added a secret for the tunnel.

--- flake.nix
+++ flake.nix
@@ -9,49 +9,63 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, kubernetes, nixops, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        k8s  = kubernetes.lib;
-      in {
-        packages.traefikManifests = let
-          traefikRelease = k8s.helm.release {
-            name = "traefik";
-            namespace = "kube-system";
-            chart = "traefik";
-            repo  = "https://helm.traefik.io/traefik";
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+      k8s  = kubernetes.lib;
+    in {
+      packages.traefikManifests = let
+        traefikRelease = k8s.helm.release {
+          name = "traefik";
+          namespace = "kube-system";
+          chart = "traefik";
+          repo  = "https://helm.traefik.io/traefik";
             version = "37.0.0"; # updateable
-            values = {
-              service = { type = "NodePort"; };
-              deployment = { kind = "DaemonSet"; };
-              ingressClass = { enabled = true; isDefaultClass = true; };
-            };
+          values = {
+            service = { type = "NodePort"; };
+            deployment = { kind = "DaemonSet"; };
+            ingressClass = { enabled = true; isDefaultClass = true; };
           };
-        in traefikRelease.resources;
-
-        packages.cloudflaredManifests = let
-          cloudflaredRelease = k8s.helm.release {
-            name = "cloudflared";
-            namespace = "kube-system";
-            chart = "cloudflare/cloudflared";
-            repo  = "https://cloudflare.github.io/helm-charts";
-            version = "0.2.0"; # example, check latest
-            values = {
-                tunnel = { 
-                    # Replace with your tunnel name and credentials secret
-                    name = "my-tunnel";  
-                };
-                credentials = {
-                    existingSecret = "cloudflared-credentials"; 
-                };
-            };
-          };
-        in cloudflaredRelease.resources;
-
-        # A nixopsConfigurations entry which we will override/populate with a generated "network.nix"
-        nixopsConfigurations.k3sHetzner = {
-          network = import ./network.nix;
         };
-      }
-    );
-}
+      in traefikRelease.resources;
+
+      packages.cloudflaredManifests = let
+        cloudflaredRelease = k8s.helm.release {
+          name = "cloudflared";
+          namespace = "kube-system";
+          chart = "cloudflared";
+          repo  = "https://cloudflare.github.io/helm-charts";
+            version = "0.2.0"; # example, check latest
+          values = {
+            tunnel = { name = "ingress-tunnel"; };
+            credentials = { existingSecret = "cloudflared-credentials"; };
+          };
+        };
+      in cloudflaredRelease.resources;
+
+      cloudflaredCredentialsSecret = k8s.lib.mkSecret {
+        metadata = {
+          name = "cloudflared-credentials";
+          namespace = "kube-system";
+        };
+        stringData = {
+          "credentials.json" = ''
+            {
+              "AccountTag": "your-account-tag",
+              "TunnelSecret": "your-tunnel-secret",
+              "TunnelID": "your-tunnel-id",
+              "TunnelName": "my-tunnel"
+            }
+          '';
+        };
+        type = "Opaque";
+      };
+
+      packages.k8sResources = self.packages.traefikManifests ++ self.packages.cloudflaredManifests ++ [ self.cloudflaredCredentialsSecret ];
+
+      nixopsConfigurations.k3sHetzner = {
+        network = import ./network.nix;
+        # reference k8sResources for deployment here
+      };
+    }
+  );
