428a2a042bb64f1c635e458eeaae19b584e0f93f
Yunior <yunior.alayo@gmail.com>
2025-08-10 10:15:27 +0200

    Fixing output variables names.

--- scripts/make-nixops-network.sh
+++ scripts/make-nixops-network.sh
@@ -7,8 +7,8 @@ out=network.nix
 
 jq -r '
   {
-    server: .server_ip,
-    workers: .worker_ips
+    server: .first_node_ip,
+    workers: .nodes_ips
   }' "$infile" > /tmp/inventory.json
 
 SERVER_IP=$(jq -r .server /tmp/inventory.json)
