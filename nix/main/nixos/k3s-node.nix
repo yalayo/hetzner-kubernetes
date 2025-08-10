afcf22ac4ee64542dc7bc27bf13ffe2e589708ba
Yunior <yunior.alayo@gmail.com>
2025-08-10 15:35:08 +0200

    Rolling back some changes.

--- nix/main/nixos/k3s-node.nix
+++ nix/main/nixos/k3s-node.nix
@@ -1,26 +1,6 @@
 { config, pkgs, lib, k3s ? { token = ""; }, ... }: # Accept k3s.token as flake arg
 
-let
-  helmBinary = pkgs.helm;
-  # Helm chart versions can be fixed here or overridden
-  traefikChartVersion = "37.0.0"; 
-  cloudflaredChartVersion = "1.7.1";
-
-  # Helm install commands as scripts
-  installTraefik = ''
-    ${helmBinary}/bin/helm upgrade --install traefik traefik/traefik \
-      --namespace kube-system \
-      --set dashboard.enabled=true \
-      --set rbac.enabled=true \
-      --version ${traefikChartVersion}
-  '';
-
-  installCloudflared = ''
-    ${helmBinary}/bin/helm upgrade --install cloudflared cloudflare/cloudflared \
-      --namespace kube-system \
-      --version ${cloudflaredChartVersion}
-  '';
-in {
+{
   options.k3s = {
     token = lib.mkOption {
       type = lib.types.str;
@@ -34,7 +14,7 @@ in {
 
     services.openssh.enable = true;
 
-    environment.systemPackages = with pkgs; [ vim jq helm ];
+    environment.systemPackages = with pkgs; [ vim jq ];
 
     # Kernel modules
     boot.kernelModules = [
@@ -66,28 +46,6 @@ in {
         "--tls-san=10.1.1.1"
         "--disable=traefik"
       ];
-    };
-
-    system.activationScripts.k3sHelmDeploy = {
-      text = ''
-        # Wait for k3s to be active (adjust if needed)
-        until kubectl get nodes &> /dev/null; do sleep 3; done
-
-        # Add Helm repos if not added
-        if ! ${helmBinary}/bin/helm repo list | grep traefik; then
-          ${helmBinary}/bin/helm repo add traefik https://helm.traefik.io/traefik
-        fi
-        if ! ${helmBinary}/bin/helm repo list | grep cloudflare; then
-          ${helmBinary}/bin/helm repo add cloudflare https://cloudflare.github.io/helm-charts
-        fi
-        ${helmBinary}/bin/helm repo update
-
-        # Install Traefik
-        ${installTraefik}
-
-        # Install Cloudflared
-        ${installCloudflared}
-      '';
     };
 
     # Time
