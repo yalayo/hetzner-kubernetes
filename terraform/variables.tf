25d10b0ccba7fae6e16f25a5b2921dd2552d2e60
Yunior <yunior.alayo@gmail.com>
2025-08-25 19:07:23 +0200

    Added a default to avoid issues testing locally.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,11 @@
 ## Variable to store the ssh private key
 variable "ssh_private_key" {
   sensitive = true
+  default = ""
 }
 
 ## Variable to the k3s token
 variable "k3s_token" {
   sensitive = true
+  default = ""
 }
