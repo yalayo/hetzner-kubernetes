8a38896d4fb178a6d8b8592af1b18f338f2303f3
Yunior <yunior.alayo@gmail.com>
2026-01-04 00:21:36 +0100

    Passing some parameters to hive.

--- k3s-colmena/hive.nix
+++ k3s-colmena/hive.nix
@@ -2,20 +2,21 @@ let
   nodesJson = builtins.fromJSON (builtins.readFile ./terraform.json);
   nodeNames = builtins.attrNames nodesJson;
 
-  mkNode = name: ip: {
-    deployment = {
-      targetHost = ip;
-      targetUser = "root";
+  mkNode = name: ip: { config, lib, pkgs, ... }:
+    {
+      deployment = {
+        targetHost = ip;
+        targetUser = "root";
+      };
+
+      networking.hostName = name;
+
+      _module.args = {
+        clusterNodes = nodesJson;
+        clusterNodeNames = nodeNames;
+      };
+
+      imports = [ ./node.nix ];
     };
-
-    networking.hostName = name;
-
-    _module.args = {
-      clusterNodes = nodesJson;
-      clusterNodeNames = nodeNames;
-    };
-
-    imports = [ ./node.nix ];
-  };
 in
 builtins.mapAttrs mkNode nodesJson
