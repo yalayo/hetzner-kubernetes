274557e12f7c32394421d522e3b7e3a8ed448821
Yunior <yunior.alayo@gmail.com>
2026-01-04 00:28:46 +0100

    Trying another approach.

--- k3s-colmena/hive.nix
+++ k3s-colmena/hive.nix
@@ -2,21 +2,19 @@ let
   nodesJson = builtins.fromJSON (builtins.readFile ./terraform.json);
   nodeNames = builtins.attrNames nodesJson;
 
-  mkNode = name: ip: { config, lib, pkgs, ... }:
-    {
-      deployment = {
-        targetHost = ip;
-        targetUser = "root";
-      };
-
-      networking.hostName = name;
-
-      _module.args = {
-        clusterNodes = nodesJson;
-        clusterNodeNames = nodeNames;
-      };
-
-      imports = [ ./node.nix ];
+  mkNode = name: ip: {
+    deployment = {
+      targetHost = ip;
+      targetUser = "root";
     };
+
+
+    _module.args = {
+      clusterNodes = nodesJson;
+      clusterNodeNames = nodeNames;
+    };
+
+    imports = [ ./node.nix ];
+  };
 in
 builtins.mapAttrs mkNode nodesJson
